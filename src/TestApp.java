6deae0702e254018494b0f7c122ae253e201570f
sherwin wyco <sherwin.wyco20@gmail.com>
2019-04-05 10:40:07 -0400

    modified app name

--- src/TestApp.java
+++ src/TestApp.java
@@ -1,3 +1,4 @@
+import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
@@ -6,6 +7,7 @@ public class TestApp {
   private static int port = 7000;
 
   public static void main(String[] args) {
+
 
     try {
       sourceAddress = InetAddress.getLocalHost();
@@ -17,12 +19,28 @@ public class TestApp {
     Socket third = new Socket(port + 2, Socket.SocketType.NoBroadcast);
     Socket fourth = new Socket(port + 3, Socket.SocketType.NoBroadcast);
     Socket[] sockets = {first, second, third, fourth};
-    int i = 0;
+    int i = 1;
     for (Socket socket : sockets) {
-      socket.send("from port" + port + i, sourceAddress, 8080);
+      socket.send("from socket" + i, sourceAddress, 8080);
       i++;
     }
 
-  }
+    do {
+      for (int j = 0; j < 4; j++) {
+        DatagramPacket inPacket = sockets[j].receive();
+        if (inPacket != null) {
+          byte[] inBuffer = inPacket.getData();
+          System.out.println(inPacket.getLength());
+          String inMessage = new String(inBuffer).substring(0, inPacket.getLength());
+          InetAddress senderAddress = inPacket.getAddress();
+          int senderPort = inPacket.getPort();
+          System.out.println("Received message: " + inMessage);
 
+          // send a message back after receiving.
+          sockets[j].send(inMessage + "from socket" + j, senderAddress, senderPort);
+        }
+      }
+
+    } while (true);
+  }
 }
