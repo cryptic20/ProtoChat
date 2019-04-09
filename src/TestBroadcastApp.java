a0d759b3d1077fece3a04143d58d14d42cb4aee4
sherwin wyco <sherwin.wyco20@gmail.com>
2019-04-09 13:21:16 -0400

    modified test case for broadcast testing

--- src/TestBroadcastApp.java
+++ src/TestBroadcastApp.java
@@ -1,7 +1,6 @@
 import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
-import java.util.Arrays;
 
 public class TestBroadcastApp {
   private static InetAddress sourceAddress;
@@ -21,13 +20,14 @@ public class TestBroadcastApp {
     Socket fourth = new Socket(port + 3, Socket.SocketType.Broadcast);
     Socket[] sockets = {first, second, third, fourth};
     int i = 1;
-    for (Socket socket : sockets) {
-      socket.send("????? Scher ##### socket" + i, sourceAddress, 64000);
-      i++;
-    }
+    sockets[0].send("????? Scher ##### socket" + i, sourceAddress, 64000);
+    // for (Socket socket : sockets) {
+    // socket.send("????? Scher ##### socket" + i, sourceAddress, 64000);
+    // i++;
+    // }
     int k = 1;
     do {
-      for (int j = 0; j < 4; j++) {
+      for (int j = 0; j < 1; j++) {
         DatagramPacket inPacket = sockets[j].receive();
         if (inPacket != null) {
           byte[] inBuffer = inPacket.getData();
@@ -36,16 +36,27 @@ public class TestBroadcastApp {
           int senderPort = inPacket.getPort();
           System.out.println("Received message: " + inMessage);
 
-          String[] split_message = inMessage.split(" "); // split message by white-space;
-          System.out.println(Arrays.toString(split_message));
-          if (split_message[0].equals("#####") && split_message[1].equals("socket" + k)) {
+
+          if (inMessage.contains("#####")) {
+            String[] split_message = inMessage.split(" "); // split message by white-space;
+            if (split_message[0].equals("#####") && split_message[1].equals("socket" + k)) {
+              System.out.println("it matched!");
+              try {
+                senderAddress = InetAddress.getByName(split_message[3]);
+              } catch (UnknownHostException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+              }
+            }
             // send a message back after receiving.
-            sockets[j].send("I received your message Scher, \nfrom socket" + (j + 1), senderAddress,
-                senderPort);
+            sockets[j].send("Initiating conversation..." + (j + 1), senderAddress, senderPort);
+
           } else {
-            sockets[j].send("I received your message\n" + inMessage + "\nfrom socket" + (j + 1),
+            sockets[j].send(
+                "I received your message Scher, '" + inMessage + "' \nfrom socket" + (j + 1),
                 senderAddress, senderPort);
           }
+
         }
         k++;
       }
